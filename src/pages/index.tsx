5cfbe8523cb40c97f8f7cb56dbc0b7e213efcaa6
Wilson Neto <wilsonpsn@hotmail.com>
2020-11-18 21:12:01 -0300

    removido husky

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,11 +6,8 @@ import Title from '@/components/Title';
 const Home: React.FC<PageProps> = () => (
   <main>
     <Title />
-    <p>A TypeScript starter for Gatsby. Great for advanced users.</p>
-    <p>
-      Follow me on Twitter (
-      <a href="https://twitter.com/jpedroschmitz">@jpedroschmitz</a>)
-    </p>
+    <p>Aewww</p>
+    <p>Follow me on Twitter (</p>
   </main>
 );
 
